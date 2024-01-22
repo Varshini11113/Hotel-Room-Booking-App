c7116ea36a57022e4d8c7d193ee143101a5aa14a
Varshini M <105061954+Varshini11113@users.noreply.github.com>
2024-01-22 21:40:22 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -25,7 +25,11 @@ mongoose.connection.on("disconnected", () => {
 });
 
 //middlewares
-app.use(cors())
+app.use(cors(
+  "origin" : ["https://hotel-room-booking-app-server.vercel.app/"],
+  "methods" : ["POST", "GET", "PUT"],
+  "credentials" : true
+))
 app.use(cookieParser())
 app.use(express.json());
 
